de8531c0391c1cf063c5b96b249ef51ec1993805
HuangLuGuang <835923123@qq.com>
2020-05-07 18:12:29 +0800

    Initial commit

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @createTime    : 2020/5/7 17:17
+# @author  : Huanglg
+# @fileName: __init__.py.py
+# @email: luguang.huang@mabotech.com
---
+++ utils/pdf2text/pdf2text.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @createTime    : 2020/5/7 17:22
+# @author  : Huanglg
+# @fileName: pdf2text.py
+# @email: luguang.huang@mabotech.com
